60eacb804b1593110fe98581fce5f28dbce82bb4
adrianocpr <47866496+adrianocpr@users.noreply.github.com>
2025-05-18 23:52:38 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
+from bot.core import bot
+from bot.config import DISCORD_BOT_TOKEN
 
-from fastapi import FastAPI
-from api.routes.planeta import router as planeta_router
-from api.routes.personagem import router as personagem_router
-
-app = FastAPI()
-
-app.include_router(planeta_router, prefix="/gerar_planeta", tags=["Planeta"])
-app.include_router(personagem_router, prefix="/gerar_personagem", tags=["Personagem"])
+bot.run(DISCORD_BOT_TOKEN)
